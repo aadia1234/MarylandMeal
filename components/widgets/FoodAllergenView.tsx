2e619bf91458cd2f69f1e58517dbcd9ec0c09af1
gam3boy1 <aadianand1234@gmail.com>
2025-03-07 15:40:16 -0500

    fixed key error issues with menu page

--- components/widgets/FoodAllergenView.tsx
+++ components/widgets/FoodAllergenView.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { HStack } from "@/components/ui/hstack";
-import { Text } from "@/components/ui/text";
-import { VStack } from "@/components/ui/vstack";
-import { Heading } from "@/components/ui/heading";
-import { Divider } from "@/components/ui/divider";
-import { View } from "@/components/ui/view";
-import Allergen, { allergens } from "@/interfaces/Allergen";
-import { createIcon, Icon } from "@/components/ui/icon";
-import { Path } from "react-native-svg";
-import { Center } from "../ui/center";
+import { allergens } from "@/interfaces/Allergen";
 import AllergenView from "./AllergenView";
 
 export default function FoodAllergenView({ data }: { data?: string[] }) {
@@ -18,7 +10,7 @@ export default function FoodAllergenView({ data }: { data?: string[] }) {
         <HStack space="xs" className="my-auto flex flex-row  items-center w-full justify-center">
             {foodAllergens.map((allergen, index) => {
                 return (
-                    <AllergenView size="sm" {...allergen} />
+                    <AllergenView key={index} size="sm" {...allergen} />
                 );
             })}
         </HStack>
