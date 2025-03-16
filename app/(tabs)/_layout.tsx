82c714ee43a587fd76976ee8c6de7fbde3d3eaf5
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-03-16 19:33:34 -0400

    fixed typo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,15 @@
-import Tabbar from "@/components/navigation/Tabbar";
+import Tabbar from "@/components/navigation/TabBar";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-    return (
-        <Tabs tabBar={(props) => <Tabbar {...props} />} screenOptions={{ headerShown: false, lazy: false }} >
-            <Tabs.Screen name="home" options={{ title: "Home" }} />
-            <Tabs.Screen name="menu" options={{ title: "Menu" }} />
-            <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-        </Tabs>
-    );
+  return (
+    <Tabs
+      tabBar={(props) => <Tabbar {...props} />}
+      screenOptions={{ headerShown: false, lazy: false }}
+    >
+      <Tabs.Screen name="home" options={{ title: "Home" }} />
+      <Tabs.Screen name="menu" options={{ title: "Menu" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
 }
