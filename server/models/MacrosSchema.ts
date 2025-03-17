9784c9dad6d70a1343a7169f1621f694229aaaa2
gam3boy1 <aadianand1234@gmail.com>
2025-03-17 18:44:16 -0400

    Removed serverside code from repository

--- server/models/MacrosSchema.ts
+++
@@ -1,9 +0,0 @@
-import Macros from "@/interfaces/Macros";
-import { Schema } from "mongoose";
-
-export default new Schema<Macros>({
-  calories: Number,
-  fats: Number,
-  protein: Number,
-  carbs: Number,
-});
--- server/types/session.d.ts
+++
@@ -1,7 +0,0 @@
-import session from "express-session";
-
-declare module "express-session" {
-  export interface SessionData {
-    userId: string; // Or change the type based on your user ID format
-  }
-}
